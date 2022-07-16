d7b4d25ae95e97614e9ad339c610bd14f7088706
Glitch (jumbled-sage-lime) <none>
2022-07-16 23:39:21 +0000

    👍🔔 Checkpoint ./komutlar/yardım.js:27262435/829

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -2,7 +2,7 @@ module.exports = [{
   name:"yardım",
   code:`
  
-$description[1;Aşağıdaki menüleri kullanarak bot hakkında yardım alabilirsiniz] $addSelectMenu[1;yardım;Menülere tıkla ve komutlar hakkında bilgi al!;1;1;false;Ayarlamalı:Ayarlamalı komutları görmek için tıkla.:0:false:⚽;Moderasyon:Moderasyon komutlarını görmek için tıkla.:1:false:⚽;Eğlence:Eğlence komutlarını görmek için tıkla.:2:false:⚽;Genel:Genel komutları görmek için tıkla.:3:false:⚽] $color[1;RANDOM] $thumbnail[1;$userAvatar[$authorID]]
+$description[1;Aşağıdaki menüleri kullanri kullanarak bot hakkında yardım alabilirsiniz] $addSelectMenu[1;yardım;Menülere tıkla ve komutlar hakkında bilgi al!;1;1;false;Ayarlamalı:Ayarlamalı komutları görmek için tıkla.:0:false:⚽;Moderasyon:Moderasyon komutlarını görmek için tıkla.:1:false:⚽;Eğlence:Eğlence komutlarını görmek için tıkla.:2:false:⚽;Genel:Genel komutları görmek için tıkla.:3:false:⚽] $color[1;RANDOM] $thumbnail[1;$userAvatar[$authorID]]
   `
   },{
     name:"yardım",
